95e34db91945abf9baa98dedb085c893e545c8e5
arif-tegar <arif_karim_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 15:11:05 +0700

    Add files via upload

---
+++ tugas vs code/tes.java
@@ -0,0 +1,9 @@
+public class tes {
+    public static void main(String[] args) {
+        System.out.println("nama : arif tegar muhaimin karim");
+        System.out.println("alamat : madiun");
+        System.out.println("smp : MTsN 4 Madiun");
+        System.out.println("No hp : 085755623743");
+    }     
+
+}
